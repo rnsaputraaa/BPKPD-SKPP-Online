d8b53c7fbe87518b35d8e3e34578ca467ae2844a
Rama <ramanurj@gmail.com>
2025-12-16 19:45:58 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
             </svg>
-            <span class="font-medium">Profile Saya</span>
+            <span class="font-medium">Profile</span>
             <svg class="w-4 h-4 transform group-hover:translate-x-1 transition-transform" fill="none"
                 stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
